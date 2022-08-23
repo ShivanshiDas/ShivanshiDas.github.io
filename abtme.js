bb7890b399bfd4bf9089330dbbbff998482afb2c
Shivanshi Das <shivanshi.das@student.allenisd.org>
2022-08-23 12:23:43 -0500

    First commit

---
+++ abtme.js
@@ -0,0 +1,23 @@
+
+var facts = [
+    "I am a junior in highschool.",
+    "I love tiramisu.",
+    "I like playing tennis in my free time.",
+    "I love minions.",
+    "I enjoy biking in my free time.",
+    "I know 3.5 languages.",
+    "I have a younger sister.",
+    "My favorite color is pink.",
+    "I like doing mehendi (henna) in my free time.",
+    "I enjoy making art."
+  ]
+
+function newFact(){
+    var randomNumber = Math.floor(Math.random()*facts.length);
+    document.getElementById("facts").innerHTML = facts[randomNumber];
+}
+
+
+
+
+    
