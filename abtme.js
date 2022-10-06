75add8539c8358b0388dbab305dd5e9edd7c5d49
Shivanshi Das <shivanshi.das@student.allenisd.org>
2022-10-06 10:26:46 -0500

    Digital Portfolio Polish

--- abtme.js
+++ abtme.js
@@ -5,11 +5,17 @@ var facts = [
     "I like playing tennis in my free time.",
     "I love minions.",
     "I enjoy biking in my free time.",
-    "I know 3.5 languages.",
+    "I know 3.5 languages (Oriya, English, Hindi, 1/2(Spanish)).",
     "I have a younger sister.",
     "My favorite color is pink.",
-    "I like doing mehendi (henna) in my free time.",
-    "I enjoy making art."
+    "I like doing mehendi (henna) in my free time. Follow my mehendi account on Instagram @mehendibyshivanshi :)",
+    "I enjoy making art.",
+    "I have been practicing Odissi, and Indian classical dance form, since the age of 7.",
+    "I love cleaning.",
+    "I cannot cook to save my life.",
+    "I have 2 pet turtles.",
+    
+    
 ]
 
 var members = [
